04748f6aea7e0e941e3dbb95c00854d37102f29a
thnhu <hnhu3050@gmail.com>
2024-12-14 01:15:34 +0700

    header, footer, navbar

---
+++ ecomerce-phone/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Trang chủ</a></li>
+        <li><a href="#">Sản phẩm</a></li>
+        <li><a href="#">Giỏ hàng</a></li>
+        <li><a href="#">Đăng nhập</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
