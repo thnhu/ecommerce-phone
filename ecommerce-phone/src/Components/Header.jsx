4b95052752ff89a7394c44e17253980a83dfdec2
thnhu <hnhu3050@gmail.com>
2024-12-15 13:36:48 +0700

    update

--- ecommerce-phone/src/Components/Header.jsx
+++ ecommerce-phone/src/Components/Header.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import {Col, Row} from 'antd';
 function Header() {
     return (
         <div>
-            <Row>
-                <Col span={6}>col-8</Col> 
-                <Col span={12}>col-8</Col> 
-                <Col span={8}>col-8</Col>
-            </Row>
+            Thế giới di động 
         </div>
     )
 }
