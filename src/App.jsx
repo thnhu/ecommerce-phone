dafd8199db6404361d0a0720b859cf078240a1de
thnhu <hnhu3050@gmmail.com>
2025-02-06 16:03:11 +0700

    api examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import axios from 'axios';
 // import CategoryPage from "./pages/CategoryPage"
 import ProductsPage from "./pages/ProductsPage"
 import HomePage from './pages/HomePage';
@@ -27,9 +27,23 @@ const router = createBrowserRouter([
   },
 
 ]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  const [getCategory, setCategory] = useState([]);
+  useEffect(() => {
+    const getAPI = async () => {
+      const result = await axios.get("http://localhost:8080/phone/category");
+      setCategory(result.data);
+    }
+    getAPI();
+  },[])
+  // return <RouterProvider router={router} />;
+  return (
+    <>
+      <ul>
+        {getCategory.map((value) => <li key={value.id}>{value.name}</li>)}
+      </ul>
+    </>
+  )
 }
 
 export default App
