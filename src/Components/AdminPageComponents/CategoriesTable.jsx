7c3cb087eec411ab49d6db1f4fbe4a955bb7bcaf
btpm34 <pminh9050@gmail.com>
2025-02-15 21:05:45 +0700

    update CategoriesTable

--- src/Components/AdminPageComponents/CategoriesTable.jsx
+++ src/Components/AdminPageComponents/CategoriesTable.jsx
@@ -3,22 +3,22 @@ import React, {useEffect, useState} from "react";
 const CategoriesTable = () => {
     const [getCategory, setCategory] = useState([]);
     useEffect(() => {
-    const getAPI = async () => {
-      const result = await axios.get("http://localhost:8080/phone/category");
-      setCategory(result.data);
-    }
-        getAPI();
-    },[])
-  return (
-    <>
-      <ul>
-        {getCategory.map((value) => <li key={value.id}>{value.name}</li>)}
-      </ul>
-    </>
-  )
-
+        const getCategory = async () => {
+        const result = await axios.get("http://localhost:8080/phone/category");
+        setCategory(result.data);
         }
-    
+        getCategory();
+    },[])
+    return (
+        <>
+        <ul>
+            {getCategory.map((value) => <li key={value.id}>{value.name}</li>)}
+        </ul>
+        </>
+    )
+
+        
+}
     
 
 
